44e86a0049ebfda799e0fc8635d146805063bb41
Shubh Sheth <shubhsheth8@gmail.com>
2021-10-27 20:52:21 -0500

    Added Scroll View and Basic elements

--- StockTracker/ViewController.swift
+++ StockTracker/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var totalHoldingsView: UIView!
+    @IBOutlet weak var dayGainsView: UIView!
+    @IBOutlet weak var totalGainsView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.`
+        
+        // STYLES
+        // Total Holdings
+        totalHoldingsView.layer.cornerRadius = 13
+        
+        // Day Gains
+        dayGainsView.layer.cornerRadius = 13
+        totalGainsView.layer.cornerRadius = 13
     }
 
 
