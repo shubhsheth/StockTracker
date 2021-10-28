6e12237e9c6b257d381677549b44ccc8c06fe2be
Shubh Sheth <shubhsheth8@gmail.com>
2021-10-27 22:35:53 -0500

    Updated Home Elements

--- StockTracker/ViewController.swift
+++ StockTracker/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalHoldingsView: UIView!
     @IBOutlet weak var dayGainsView: UIView!
     @IBOutlet weak var totalGainsView: UIView!
+    @IBOutlet weak var dailyTrendView: UIView!
+    @IBOutlet weak var portfolioBreakdownView: UIView!
+    @IBOutlet weak var latestTradesView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,9 @@ class ViewController: UIViewController {
         totalHoldingsView.layer.cornerRadius = 13
         dayGainsView.layer.cornerRadius = 13
         totalGainsView.layer.cornerRadius = 13
+        dailyTrendView.layer.cornerRadius = 13
+        portfolioBreakdownView.layer.cornerRadius = 13
+        latestTradesView.layer.cornerRadius = 13
     }
 
 
